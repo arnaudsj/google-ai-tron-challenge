d99ad223ddb315b1b27b78007793786381c84823
Sébastien Arnaud <arnaudsj@gmail.com>
2010-02-17 21:50:08 -0600

    First commit in the js branch

---
+++ MyTronBot.js
@@ -0,0 +1,111 @@
+// Template for your tron bot
+var sys = require("sys");
+var tron = require('./lib/tron');
+
+var DEBUG = false;
+
+/* Next test :)
+function negascout(node, depth, α, β)
+    if node is a terminal node or depth = 0
+        return the heuristic value of node
+    b := β                                          (* initial window is (-β, -α) *)
+    foreach child of node
+        a := -negascout (child, depth-1, -b, -α)
+        if a>α
+            α := a
+        if α≥β
+            return α                                (* Beta cut-off *)
+        if α≥b                                      (* check if null-window failed high*)
+           α := -negascout(child, depth-1, -β, -α)  (* full re-search *)
+           if α≥β
+               return α                             (* Beta cut-off *)    
+        b := α+1                                    (* set new null window *)             
+    return α
+*/
+
+function randomChoice(a) {
+    return a[Math.floor(Math.random() * a.length)];
+}
+
+var gnode = function(stateb) {    
+    this.stateboard = new tron.stateboard(stateb);
+    
+    this.is_terminal = function() {
+        if(this.stateboard.moves("them").length==0 || this.stateboard.moves("me").length==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    this.heuristic = function() {
+        // Making basic heuristic based on mobility only for now
+        var utility = -Infinity;
+    
+        if(this.stateboard.me==this.stateboard.them 
+            || (this.stateboard.moves("them").length==0 
+            && this.stateboard.moves("me").length==0)) utility = 0; // Did we collide or no option
+        if(this.stateboard.moves("them").length==0) utility = 10; // Is P2 without option next turn
+        if(this.stateboard.moves("me").length==0) utility = -10; // Is P1 without option next turn?
+
+        return utility;
+    }
+    
+    this.children = function() {
+        var c = this.stateboard.possible_moves()
+        if (DEBUG) {
+            for (i=0; i<c.length; i++) {
+                sys.puts("debug (new child): "+ c[i].state)
+            }
+        }   
+        return c;
+    }
+
+
+}
+
+function negamax(node, depth) {
+
+    
+    if (DEBUG) {
+        node.stateboard.print();
+        sys.puts(node.is_terminal()+":"+depth);
+    }
+    
+    if (node.is_terminal() || depth <= 0) {
+        /*if (DEBUG) node.stateboard.print();*/
+        if (DEBUG) sys.puts("heuristic:"+node.heuristic());
+        return {"alpha": node.heuristic(), "move": null}
+    }
+    var alpha = -Infinity;
+    var best_move = 0;
+    var node_children = node.children();
+    for (var i=0; i<node_children.length; i++) {
+        var new_val = negamax(new gnode(node_children[i]), depth-1).alpha;
+        if (new_val>alpha) {
+            best_move = node_children[i].my_move;
+            alpha = new_val; 
+            }
+    }
+
+    if (DEBUG) sys.puts("heuristic(alpha):"+alpha+" (depth:"+depth+") move => "+best_move);
+    return {"alpha":alpha, "move": best_move};
+}
+
+function which_move(board) {
+    var StateBoard = {"w":board.width, "h":board.height, "state": board.state,
+         "me": board.me(), "them": board.them()}  
+    var RootNode = new gnode(StateBoard);
+    
+    var negamax_var = negamax(RootNode, 4);
+    //if (DEBUG) sys.puts(best_move);
+
+    var my_best_move = (negamax_var.move==0||negamax_var.move==null)?randomChoice(RootNode.stateboard.moves("me")):negamax_var.move;
+
+    if (DEBUG) sys.puts("NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4");
+    sys.puts(my_best_move);
+    
+
+}
+
+tron.play(which_move);
--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,18 @@
 from fabric.api import run, env
-from compileall import compile_dir
 
 def prod():
     env.user = 'tronaigc'
     env.key_filename = '/Users/arnaudsj/.ssh/id_rsa_tronaigc'
         
     env.hosts = ['facebook01']
-    env.workdir = "/home/tronaigc/google-ai-tron-challenge-py"
+    env.workdir = "/home/tronaigc/google-ai-tron-challenge-js"
     env.distdir = "/tmp/google-entry.zip"
     
     env.testdir = "/home/tronaigc/test-entry"
 
 def local():
     env.hosts = ["aura"]
-    env.workdir = "/Users/arnaudsj/Development/Github/google-ai-tron-challenge-py"
+    env.workdir = "/Users/arnaudsj/Development/Github/google-ai-tron-challenge-js"
     env.distdir = "/Users/arnaudsj/Temp/google-entry.zip"
     env.testdir = "/tmp"
 
@@ -27,18 +26,15 @@ def pull():
     run('cd %s; git pull origin master' % env.workdir) #; git submodule update
 
 def trun():
-    run('cd %s; java -jar engine/Tron.jar maps/mytest-board.txt "python MyTronBot.py" "java -jar example_bots/Chaser.jar"' % env.workdir)
+    run('cd %s; java -jar engine/Tron.jar maps/ring.txt "node MyTronBot.js" "java -jar example_bots/Chaser.jar"' % env.workdir)
 
 def package():
-    compile_dir("%s/lib/diesel" % env.workdir, force=1)
-    compile_dir("%s/lib/tronlib" % env.workdir, force=1)
-    
-    run('cd %s/build/python; rm -Rf *' % env.workdir)
-    run('cd %s; cp -R engine build/python/' % env.workdir)
-    run('cd %s; cp -R example_bots build/python/' % env.workdir)
-    run('cd %s; cp -R lib build/python/' % env.workdir)
-    run('cd %s; cp -R maps build/python/' % env.workdir)
-    run('cd %s; cp -R MyTronBot.py build/python/' % env.workdir)
+    run('cd %s/build/javascript; rm -Rf *' % env.workdir)
+    run('cd %s; cp -R engine build/javascript/' % env.workdir)
+    run('cd %s; cp -R example_bots build/javascript/' % env.workdir)
+    run('cd %s; cp -R lib build/javascript/' % env.workdir)
+    run('cd %s; cp -R maps build/javascript/' % env.workdir)
+    run('cd %s; cp -R MyTronBot.js build/javascript/' % env.workdir)
     
     try:
         run("rm %s" % env.distdir)
@@ -49,11 +45,11 @@ def package():
 def testdist():
     package()
     try:
-        run("rm -Rf %s/python" % env.testdir)
+        run("rm -Rf %s/javascript" % env.testdir)
     except:
         pass
     run("cd %s; unzip %s" % (env.testdir, env.distdir))
-    run('cd %s/python; java -jar engine/Tron.jar maps/mytest-board.txt "python MyTronBot.py" "java -jar example_bots/Chaser.jar"' % env.testdir)
+    run('cd %s/javascript; java -jar engine/Tron.jar maps/ring.txt "python MyTronBot.py" "java -jar example_bots/Chaser.jar"' % env.testdir)
     
 
 def test():
